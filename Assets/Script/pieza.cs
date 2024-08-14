bcce6a2d03f13f8efd0014d04615b8c5c1990910
JosueRomeroQuesada <107214540+JosueRomeroQuesada@users.noreply.github.com>
2024-08-13 21:55:48 -0600

    Intento de puzzle

---
+++ Assets/Script/pieza.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class pieza : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Fondo;
+
+    private Vector3 PosicionCorrecta;
+    public bool Encajada;
+    public bool Seleccionada;
+
+    void Start()
+    {
+        PosicionCorrecta = transform.position;
+         transform.position = new Vector3(Random.Range(5f, 11f), Random.Range(2.5f, -7));
+    }
+    
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, PosicionCorrecta) < 0.5f)
+        {
+            if (!Seleccionada)
+            {
+                if (Encajada == false)
+                {
+                    transform.position = PosicionCorrecta;
+                    Encajada = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                    Camera.main.GetComponent<juego>().PiezasEncajadas++;
+                }
+            }
+        }
+    }
+}
