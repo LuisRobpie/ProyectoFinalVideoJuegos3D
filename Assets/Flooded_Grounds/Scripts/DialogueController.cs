c7713bdf3aef498223e2a67397d7ea56830886a1
lunacastrocamila <98502372+lunacastrocamila@users.noreply.github.com>
2024-08-27 19:40:33 -0600

    NPCs y dialogue

---
+++ Assets/Flooded_Grounds/Scripts/DialogueController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    [SerializeField]
+    Dialogue dialogue;
+
+    [SerializeField]
+    DialogueManager dialogueManager;
+
+    [SerializeField]
+    GameObject interactuarBtn; 
+
+    private bool isPlayerNearby = false; 
+
+    private void Start()
+    {
+        interactuarBtn.SetActive(false);
+    }
+
+    private void Update()
+    {
+       
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            interactuarBtn.SetActive(false); 
+            dialogueManager.StartDialogue(dialogue); 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactuarBtn.SetActive(true); 
+            isPlayerNearby = true; 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactuarBtn.SetActive(false); 
+            isPlayerNearby = false; 
+            dialogueManager.EndDialogue(); 
+        }
+    }
+}
